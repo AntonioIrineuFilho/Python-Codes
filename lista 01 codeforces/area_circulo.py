efc27ee49e3c17364ff879a6e0511003698d7df0
Antônio Irineu Filho <161607435+antchan2911@users.noreply.github.com>
2024-02-29 08:37:00 -0300

    lista codeforces 01

---
+++ lista 01 codeforces/area_circulo.py
@@ -0,0 +1,5 @@
+r=float(input())
+pi=3.14159
+area=r*r*pi
+area_form="{:.4f}".format(area)
+print("A=",area_form, sep="")
---
+++ lista 01 codeforces/media_ponderada1.py
@@ -0,0 +1,6 @@
+a,b=input().split()
+n1=int(a)
+n2=int(b)
+media=(n1*2+n2*3)/5
+media_form=int(media)
+print(media_form)

3eb031b6e05141fe600049f836805981c0526773
Antônio Irineu Filho <161607435+antchan2911@users.noreply.github.com>
2024-03-07 10:19:00 -0300

    Update piso.py

--- lista 02 codeforces/piso.py
+++ lista 02 codeforces/piso.py
@@ -1,10 +1,6 @@
 L = int(input())
 C = int(input())
-Area = L * C
-t1 = 0.5
-t2 = t1 / 2
-t3 = t2 / 2
-lajotas_t1 = (Area - 4*t3) // t1
-lajotas_t2 = ((Area - 4*t3) - lajotas_t1 * t1) // t2
-print(int(lajotas_t1))
-print(int(lajotas_t2))
+t1 = L * C + ((L-1) * (C-1))
+t2 = (2 * (L-1)) + (2 * (C-1))
+print(t1)
+print(t2)

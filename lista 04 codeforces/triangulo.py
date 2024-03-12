01bb1f93b74be06a123c644c78d40cee086c7df2
Antônio Irineu Filho <161607435+antchan2911@users.noreply.github.com>
2024-03-12 12:24:52 -0300

    Update triangulo.py

--- lista 04 codeforces/triangulo.py
+++ lista 04 codeforces/triangulo.py
@@ -1,19 +1,11 @@
 a, b, c, d = map(int, input().split())
-if (a < b or a < c or a < d):
-    if (b < c or b < d):
-        if ((a + b) < c or (a + b) < d):
-            print("S")
-    elif (c < b or c < d):
-        if ((a + c) < b or (a + c) < d):
-            print("S")
-elif (b < c or b < d):
-    if(c < d):
-        if ((b + c) < a or (b + c) < d):
-            print("S")
-    elif (d < c):
-        if ((b + d) < a or (b + d) < c):
-            print("S")
-elif ((c + d) < a or (c + d) < b):
+if ((a + b) > c and (b + c) > a and (c + a) > b):
+    print("S")
+elif ((a + b) > d and (a + d) > b and (b + d) > a):
+    print("S")
+elif((b + c) > d and (b + d) > c and (c + d) > b):
+    print("S")
+elif ((a + c) > d and (a + d) > c and (c + d) > a):
     print("S")
 else:
     print("N")

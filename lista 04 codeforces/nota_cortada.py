079d32f768942bdf9942ddf03948618c3e998dd5
Antônio Irineu Filho <161607435+antchan2911@users.noreply.github.com>
2024-03-20 12:41:04 -0300

    Update nota_cortada.py

--- lista 04 codeforces/nota_cortada.py
+++ lista 04 codeforces/nota_cortada.py
@@ -1,9 +1,9 @@
 B = int(input())
 T = int(input())
-A_esq = ((B + T) * 70) // 2
-if (A_esq == 5.600):
+A_esq = ((B + T) * 70) / 2
+if (A_esq == 5600):
     print("0")
-elif (A_esq > 5.600):
+elif (A_esq > 5600):
     print("1")
 else:
     print("2")

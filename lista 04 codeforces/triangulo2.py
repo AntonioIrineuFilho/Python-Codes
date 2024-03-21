79cdfb60ec8f2fc57964af825f34260fd19d59a1
Antônio Irineu Filho <161607435+antchan2911@users.noreply.github.com>
2024-03-21 07:33:53 -0300

    Update triangulo2.py

--- lista 04 codeforces/triangulo2.py
+++ lista 04 codeforces/triangulo2.py
@@ -1,20 +1,24 @@
 a, b, c = map(int, input().split())
 if not ((a + b) > c and (a + c) > b and (b + c) > a):
     print("n")
-elif ((a == b and a == c and c == b) or (a == b or a == c or b == c)):
-    print("a")
-elif (a > b and a > c):
-    if ((b ** 2 + c ** 2) == a ** 2):
+elif (a >= b and a >= c):
+    if ((b ** 2 + c ** 2 - a ** 2) == 0):
         print("r")
-    elif (b != c):
+    elif ((b ** 2 + c ** 2 - a ** 2) > 0):
+        print("a")
+    else:
         print("o")
-elif (b > a and b > c):
-    if ((a ** 2 + c ** 2) == b ** 2):
+elif (b >= a and b >= c):
+    if ((a ** 2 + c ** 2 - b ** 2) == 0):
         print("r")
-    elif (a != c):
+    elif ((a ** 2 + c ** 2 - b ** 2) > 0):
+        print("a")
+    else:
         print("o")
-elif (c > a and c > b):
-    if ((a ** 2 + b ** 2) == c ** 2):
+elif (c >= a and c >= b):
+    if ((a ** 2 + b ** 2 - c ** 2) == 0):
         print("r")
-    elif (a != b):
+    elif ((a ** 2 + b ** 2 - c ** 2) > 0):
+        print("a")
+    else:
         print("o")

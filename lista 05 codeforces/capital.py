93826fbd244a7ffb3b17334c2c55a848dd7aa3ca
Antônio Irineu Filho <161607435+antchan2911@users.noreply.github.com>
2024-03-25 11:35:20 -0300

    Update capital.py

--- lista 05 codeforces/capital.py
+++ lista 05 codeforces/capital.py
@@ -1,7 +1,5 @@
 A1, A2, A3, A4 = map(int, input().split())
-L1 = A1 ** (1/2)
-L4 = A4 ** (1/2)
-if (L1 * L4 == A2 and L1 * L4 == A3):
+if (A1 * A4 == A2 * A3 or A1 * A3 == A2 * A4 or A1 * A2 == A3 * A4):
     print("S")
 else:
     print("N")

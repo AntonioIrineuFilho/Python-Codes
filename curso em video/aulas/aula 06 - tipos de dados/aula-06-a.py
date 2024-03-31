c0d043446b0e9a82f283508f921041bf1ab60efd
Antônio Irineu Filho <161607435+antchan2911@users.noreply.github.com>
2024-03-31 20:07:48 -0300

    create

---
+++ curso em video/aulas/aula 06 - tipos de dados/aula-06-a.py
@@ -0,0 +1,14 @@
+# output -> str, uma vez que, caso o tipo de dado não seja especificado, ele será uma string
+n1 = input('Digite um número: ')
+print(type(n1))
+# output -> int, pois foi o tipo especificado
+n2 = int(input('Digite um número: '))
+print(type(n2))
+# output -> concatenação(junção de strings)
+n3 = input('Digite um número: ')
+n4 = input('Digite um número: ')
+print(n3 + n4)
+# output -> soma(soma de numeros inteiros)
+n5 = int(input('Digite um número: '))
+n6 = int(input('Digite um número: '))
+print(n5 + n6)

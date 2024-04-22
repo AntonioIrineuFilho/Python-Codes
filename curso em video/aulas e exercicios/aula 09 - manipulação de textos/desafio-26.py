79808954fb14f3ce134b08616418bdae87ab22e9
Antônio Irineu Filho <161607435+antchan2911@users.noreply.github.com>
2024-04-22 00:17:09 -0300

    Update desafio-26.py

--- curso em video/aulas e exercicios/aula 09 - manipulação de textos/desafio-26.py
+++ curso em video/aulas e exercicios/aula 09 - manipulação de textos/desafio-26.py
@@ -1,18 +1,18 @@
-x = input('Digite uma frase: ')
-if (x.upper().count('A') == 0):
+x = input('Digite uma frase: ').strip().upper()
+if (x.count('A') == 0):
     print('A frase não possui letras "A"')
 else:
-    if (x.upper().count('A') == 1):
-        print('A letra "A" aparece {} vez na frase'.format(x.upper().count('A')))
-        print('A letra "A" aparece pela primeira vez na posição {} da frase'.format(x.upper().strip().find('A')))
-        for i in range(len(x.strip())):
-            if (x[i].upper() == 'A'):
+    if (x.count('A') == 1):
+        print('A letra "A" aparece {} vez na frase'.format(x.count('A')))
+        print('A letra "A" aparece pela primeira vez na posição {} da frase'.format(x.find('A')))
+        for i in range(len(x)):
+            if (x[i] == 'A'):
                 y = i
         print('A letra "A" aparece pela última vez na posição {} da frase'.format(y))
     else:
-        print('A letra "A" aparece {} vezes na frase'.format(x.upper().count('A')))
-        print('A letra "A" aparece pela primeira vez na posição {} da frase'.format(x.upper().strip().find('A')))
-        for i in range(len(x.strip())):
-            if (x[i].upper() == 'A'):
+        print('A letra "A" aparece {} vezes na frase'.format(x.count('A')))
+        print('A letra "A" aparece pela primeira vez na posição {} da frase'.format(x.find('A')))
+        for i in range(len(x)):
+            if (x[i] == 'A'):
                 y = i
         print('A letra "A" aparece pela última vez na posição {} da frase'.format(y))

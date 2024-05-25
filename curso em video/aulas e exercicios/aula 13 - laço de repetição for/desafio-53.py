044215e749385344a2d64949a56c5a4ab7680a3b
Antônio Irineu Filho <161607435+antchan2911@users.noreply.github.com>
2024-05-25 20:31:48 -0300

    update

--- curso em video/aulas e exercicios/aula 13 - laço de repetição for/desafio-53.py
+++ curso em video/aulas e exercicios/aula 13 - laço de repetição for/desafio-53.py
@@ -1,10 +1,28 @@
-x = input('Digite uma frase: ').upper().split()
+# x = input('Digite uma frase: ').upper().split()
+# y = ''.join(x)
+# li = []
+# for i in range(len(y)):
+#    if (y[i] == y[len(y)-(i+1)]):
+#        li.append(y[i])
+# if (len(li) == len(y)):
+#    print('A frase digitada é um palíndromo')
+# else:
+#   print('A frase digitada não é um palíndromo')
+
+x = input('Digite uma frase: ').strip().upper().split()
 y = ''.join(x)
-li = []
-for i in range(len(y)):
-    if (y[i] == y[len(y)-(i+1)]):
-        li.append(y[i])
-if (len(li) == len(y)):
-    print('A frase digitada é um palíndromo')
+s = 0
+
+if ((len(y) + 1) % 2 == 0):
+    for i in range(0, (len(y)-1)//2):
+        if (y[i] == y[len(y)-(i+1)]):
+            s = s + 1
 else:
-    print('A frase digitada não é um palíndromo')
+    for i in range(0, len(y)//2):
+        if (y[i] == y[len(y)-(i+1)]):
+            s = s + 1
+
+if (s == (len(y)-1)/2 or s == len(y)/2):
+    print('A frase digitada é um palindromo')
+else:
+    print('A frase digitada não é um palindromo')
--- curso em video/aulas e exercicios/aula 13 - laço de repetição for/desafio-54.py
+++ curso em video/aulas e exercicios/aula 13 - laço de repetição for/desafio-54.py
@@ -1,9 +1,23 @@
-x = 0
-y = 0
-for i in range (0, 7):
-    n = int(input('Digite uma idade: '))
-    if (n < 21):
-        x = x + 1
+# x = 0
+# y = 0
+# for i in range (0, 7):
+#    n = int(input('Digite uma idade: '))
+#   if (n < 21):
+#       x = x + 1
+#    else:
+#        y = y + 1
+# print('Existem {} menores de idade e {} maiores de idade'.format(x, y))
+
+menor = 0
+maior = 0
+
+for i in range(0, 6):
+    y, m, d = map(int, input('Digite o ano, o mês e o dia de nascimento: ').split())
+    nasc = (y*365) + (m*30) + d
+    data_atual = (2024*365) + (5*30) + 25
+    if ((data_atual - nasc) / 365 < 21):
+        menor = menor + 1
     else:
-        y = y + 1
-print('Existem {} menores de idade e {} maiores de idade'.format(x, y))
+        maior = maior + 1
+
+print('{} são de menor e {} são de maior'.format(menor, maior))

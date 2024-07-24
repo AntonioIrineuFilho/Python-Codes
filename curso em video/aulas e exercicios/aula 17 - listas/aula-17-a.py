d884128f0091f6dd5c9c2f2f61f5496a9953542c
Antônio Irineu Filho <161607435+antchan2911@users.noreply.github.com>
2024-07-24 14:27:42 -0300

    upate

---
+++ curso em video/aulas e exercicios/aula 17 - listas/aula-17-a.py
@@ -0,0 +1,12 @@
+# metodos das listas:
+# .append(x) -> adiciona item no final da lista
+# .insert(indice, x) -> adiciona o item na posição do indice e passa pra frente os itens pospostos
+# sum(lista) -> soma todos os termos da lista
+# len(lista) -> tamanho da lista(quant itens)
+# del(lista[indice]) -> remove da lista pelo indice
+# .remove(x) -> remove o item pelo seu alor
+# .pop(indice) -> remove o valor pelo indice, se não especificar remove o ultimo item
+# .count(x) -> mostra a quantidade de x na lista
+# .index(x, (a partir de indice)) -> mostra o index de x a partir de uma posição especiica(mostra a primeira aparição caso não especifique)
+# .sort() -> ordena a lista de modo crescente 
+# .sort(reverse=True) -> ordena de modo decrescente
---
+++ curso em video/aulas e exercicios/aula 17 - listas/aula-17-b.py
@@ -0,0 +1,2 @@
+# é possivel construir listas utilizando range
+# lista = list(range(4, 11)) -> lista = [4, 5, 6, 7, 8, 9, 10]

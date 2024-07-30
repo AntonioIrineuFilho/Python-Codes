3af4cb6ff232a88851c81fc2bbf45870b0eed8b5
Antônio Irineu Filho <161607435+antchan2911@users.noreply.github.com>
2024-07-30 12:42:19 -0300

    Create paridade-relacoes.py

---
+++ paridade-relacoes.py
@@ -0,0 +1,16 @@
+s = [1, 2, 3, 4]
+p_cart = []
+paridade = []
+
+for i in s:
+    for j in s:
+        if ((i, j) not in p_cart):
+            p_cart.append((i, j))
+
+for (i, j) in p_cart:
+    if (i % 2 == 0 and j % 2 == 0):
+        paridade.append((i, j))
+    if (i % 2 != 0 and j % 2 == 0):
+        paridade.append((i, j))
+
+print(paridade)

7d65096b4d386d188e202199f23374cf1f2878ff
Antônio Irineu Filho <161607435+antchan2911@users.noreply.github.com>
2024-09-18 13:14:27 -0300

    update

--- enhaced_media.py
+++
@@ -1,5 +0,0 @@
-n1 = float(input("Digite sua nota 1: "))
-n2 = float(input("Digite sua nota 2: "))
-media = (n1*2 + n2*3)/5
-media_form = "{:.1f}".format(media)
-print("Sua média é", media_form)
--- media.py
+++
@@ -1,4 +0,0 @@
-x=float(input())
-y=float(input())
-media=(x*2 + y*3) / 5
-print("{:.1f}".format(media))

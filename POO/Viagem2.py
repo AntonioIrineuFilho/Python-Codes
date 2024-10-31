cfee3f6a722d871eb9b49f0c37acf9af84297971
Antônio Irineu Filho <161607435+antchan2911@users.noreply.github.com>
2024-10-30 21:20:46 -0300

    update

--- POO/Viagem2.py
+++ POO/Viagem2.py
@@ -20,7 +20,39 @@ class Travel:
         return self.__d / self.__t
     
 class UI:
+    def printGet(viagem):
+        print(f'Distância = {viagem.getDist()}\nTempo = {viagem.getTempo()}')
+    def set(viagem):
+        distancia = float(input("Digite um novo valor para a distância: "))
+        tempo = float(input("Digite um novo valor para o tempo: "))
+        viagem.setDist(distancia)
+        viagem.setTempo(tempo)
+    def printVm(viagem):
+        print(f'Velocidade média = {viagem.velocidadeMedia():.1f} km/h')
     def main():
         distancia = float(input("Digite a distância da viagem: "))
         tempo = float(input("Digite o tempo da viagem: "))
+        viagem1 = Travel(distancia, tempo)
+        ops = (1, 2, 3, 4)
+        while True:
+            print("1-Ver valores\n2-Alterar valores\n3-Calcular velocidade média\n4-Sair")
+            op = int(input("Digite a opção desejada: "))
+            while True:
+                if (op in ops):
+                    break
+                else:
+                    op = int(input("Opção inválida. Digite a opção desejada: "))
+            if (op == 1):
+                UI.printGet(viagem1)
+            elif (op == 2):
+                UI.set(viagem1)
+            elif (op == 3):
+                UI.printVm(viagem1)
+            else:
+                break
+
+UI.main()
+
+                
+
         
